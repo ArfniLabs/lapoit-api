6704c582771772788717cc246dcd66d23be46590
최재필 <jpp877@naver.com>
2025-12-10 00:23:01 +0900

    feat : 프로젝트 초기세팅 완료

---
+++ src/test/java/com/lapoit/api/LapoitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapoit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapoitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
